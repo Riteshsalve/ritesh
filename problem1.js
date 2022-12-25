0e54f954d1060302d8c9a77bf7bac99f5ca9484a
ritesh salve <riteshsalve07@gmail.com>
2022-12-25 10:21:46 +0530

    problem1.js

---
+++ problem1.js
@@ -0,0 +1,8 @@
+// If a person is allowed to drive in India print "Apply for a license" or "NA".
+let person = 18;
+if(person>=18){
+  console.log("Apply for license");
+}
+else{
+  console.log("NA");
+}
