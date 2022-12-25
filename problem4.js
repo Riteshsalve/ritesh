a5040ed2b37249d3c791bccb094369058d261477
ritesh salve <riteshsalve07@gmail.com>
2022-12-25 10:47:17 +0530

    problem4.js

---
+++ problem4.js
@@ -0,0 +1,12 @@
+// Given stored username and password and input username and password, Print if the user can login or not.//
+
+let credentials_username = "Riteshsalve";
+let credentials_password = "Ritesh123";
+let username = "Riteshsalve";
+let password = "Ritesh123";
+
+if(credentials_username == username){
+  if(credentials_password == password){
+    console.log("i can login");
+  }
+}
